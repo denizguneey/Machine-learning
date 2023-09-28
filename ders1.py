d0e8fd4b880707bc527e6a99672d585d7a38cef8
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-09-28 23:04:55 +0300

    first commit

---
+++ ders1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#veri yukleme
+#/Users/denizguney/Desktop/MachineeLearning/ aynı dosya içine kaydetmezsen
+
+veriler= pd.read_csv('veriler.csv') 
+
+#veri onisleme
+boy=veriler[['boy']]
+print(boy)
+
+boykilo=veriler[['boy','kilo']]
+print(boykilo)

a04d2b827568bfea761b4848042e3858a14d70fc
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-09-29 22:10:11 +0300

    added lesson

---
+++ lesson5.py
@@ -0,0 +1,57 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+veriler = pd.read_csv('eksikveriler.csv')
+
+print(veriler)
+
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+Yas = veriler.iloc[:,1:4].values
+print(Yas)
+imputer = imputer.fit(Yas[:,1:4])
+Yas[:,1:4] = imputer.transform(Yas[:,1:4])
+print(Yas)
+
+ulke=veriler.iloc[:,0:1].values
+print(ulke)
+
+# the process of numbering categorical data
+
+from sklearn import preprocessing
+
+le=preprocessing.LabelEncoder()
+ulke[:,0]=le.fit_transform(veriler.iloc[:,0])
+print(ulke)
+
+ohe=preprocessing.OneHotEncoder()
+ulke=ohe.fit_transform(ulke).toarray()
+print(ulke)
+
+#After converting category type data into numerical data, we combined the data separately
+sonuc=pd.DataFrame(data=ulke, index=range(22),columns=['fr','tr','us'])
+print(sonuc)
+sonuc2=pd.DataFrame(data=Yas, index=range(22), columns=['boy','kilo','yas'])
+print(sonuc2)
+
+cinsiyet= veriler.iloc[:,-1].values
+print(cinsiyet)
+
+sonuc3=pd.DataFrame(data=cinsiyet,index=range(22),columns=['cinsiyet'])
+print(sonuc3)
+
+s=pd.concat([sonuc,sonuc2],axis=1)
+print(s)
+
+s2=pd.concat([s,sonuc3],axis=1)
+print(s2)
+
+#data split process
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(s,sonuc3,test_size=0.33, random_state=0)
+

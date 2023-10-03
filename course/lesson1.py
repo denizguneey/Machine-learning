fdd481ecacf49f5022963956708160ac15c31147
Deniz <146165758+denizguneey@users.noreply.github.com>
2023-10-03 23:20:38 +0300

    edit

--- course/lesson1.py
+++ course/lesson1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #veri yukleme
 #/Users/denizguney/Desktop/MachineeLearning/ aynı dosya içine kaydetmezsen
 
-veriler= pd.read_csv('veriler.csv') 
+veriler= pd.read_csv('datasources/veriler.csv') 
 
 #veri onisleme
 boy=veriler[['boy']]
